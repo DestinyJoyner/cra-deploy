09bea08feb584303d8c289f5bafcabaab12c7156
DestinyJoyner <destinyjoyner@pursuit.org>
2022-10-14 12:43:13 -0400

    added Contact Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Contact from './Contact';
 
 function App() {
   console.log(process.env.REACT_APP_API_KEY)
@@ -7,6 +8,7 @@ function App() {
       <h1>Pursuit App Template</h1>
       <h2>My New Update After Deploying!</h2>
       <h3>Creating new branch and making changes</h3>
+      <Contact />
     </div>
   );
 }
